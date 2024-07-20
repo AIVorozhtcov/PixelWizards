8c0001598c858054a99c160d1d8323165d54e8aa
Mikhail Khmelnitskiy <xmelnickij.m.a@yandex.ru>
2024-07-20 14:30:55 +0300

    зря удалил

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,12 @@
+import Test from './Test';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hello world
+      <Test />
+    </div>
+  );
+}
+
+export default App;
