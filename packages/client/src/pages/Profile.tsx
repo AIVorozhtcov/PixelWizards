9c5592bf7cf67414829e778f00f2e1cc42a4cdd9
ZuevaDarya <zuevadasha55@gmail.com>
2024-07-22 23:03:02 +0300

    написала компонент профиля

--- packages/client/src/pages/Profile.tsx
+++ packages/client/src/pages/Profile.tsx
@@ -1,3 +1,13 @@
+import Button from '../components/atoms/Button';
+import ProfileContainer from '../components/organisms/ProfileContainer';
+
 export default function Profile() {
-  return <main className="flex flex-col min-h-dvh bg-[#0c1b2a]">Профиль</main>;
+  return (
+    <div className="relative w-full min-h-screen p-4 pt-10">
+      <Button className="absolute top-4 left-4 hover:bg-gray-300 bg-gray-200 rounded-md p-1">
+        Закрыть
+      </Button>
+      <ProfileContainer />
+    </div>
+  );
 }
