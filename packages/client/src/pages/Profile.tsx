5033a3d4cde9b5fc33eadd1a93868298ce476057
ZuevaDarya <zuevadasha55@gmail.com>
2024-07-23 21:34:01 +0300

    рефакторинг

--- packages/client/src/pages/Profile.tsx
+++ packages/client/src/pages/Profile.tsx
@@ -1,12 +1,8 @@
-import Button from '../components/atoms/Button';
-import ProfileContainer from '../components/organisms/ProfileContainer';
+import ProfileContainer from '../components/tempates/ProfileContainer';
 
 export default function Profile() {
   return (
     <div className="relative w-full min-h-screen p-4 pt-10">
-      <Button className="absolute top-4 left-4 hover:bg-gray-300 bg-gray-200 rounded-md p-1">
-        Закрыть
-      </Button>
       <ProfileContainer />
     </div>
   );
