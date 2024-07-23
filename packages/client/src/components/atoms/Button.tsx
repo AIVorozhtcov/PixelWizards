98070846e98814aa15084d81d3099955409b1454
D33Key <dimakisa777@yandex.ru>
2024-07-23 23:51:39 +0300

    feat: добавил кнопку, чтобы выйти из профиля

--- packages/client/src/components/atoms/Button.tsx
+++ packages/client/src/components/atoms/Button.tsx
@@ -1,9 +1,35 @@
+import { VariantProps, cva } from 'class-variance-authority';
+import { cn } from '../../lib/utils';
+
+const buttonVariants = cva('', {
+  variants: {
+    variant: {
+      default: '',
+      acent:
+        'bg-transparent inline-flex items-center justify-center rounded-md border border-red-600 p-2 text-sm font-medium text-white shadow transition-colors hover:bg-red-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-const Button = ({ children, ...props }: ButtonProps) => {
-  return <button {...props}>{children}</button>;
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  children: React.ReactNode;
+}
+
+const Button = ({ children, variant, className, ...props }: ButtonProps) => {
+  return (
+    <button className={cn(buttonVariants({ variant, className }))} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- packages/client/src/components/molecules/Navbar.tsx
+++ packages/client/src/components/molecules/Navbar.tsx
@@ -1,16 +1,22 @@
 import { useLocation } from 'react-router-dom';
 import arrayOfLinks from '../../constants/arrayOfLinks';
 import Link from '../atoms/Link';
+import Button from '../atoms/Button';
+import { useAuth } from '../../templates/Auth';
 
 export default function Navbar() {
   const { pathname } = useLocation();
+  const userInfo = useAuth();
   return (
-    <nav className="ml-auto flex gap-4 sm:gap-6">
+    <nav className="ml-auto flex gap-4 items-center sm:gap-6">
       {arrayOfLinks.map(link => (
         <Link key={link.href} to={link.href} isActive={pathname === link.href}>
           {link.name}
         </Link>
       ))}
+      <Button variant="acent" onClick={() => userInfo?.setUser(null)}>
+        Выйти из профиля
+      </Button>
     </nav>
   );
 }
