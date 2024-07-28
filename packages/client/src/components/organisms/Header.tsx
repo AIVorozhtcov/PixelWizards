f43a3cf11e4084874c222234946cee8b2c9669ce
ZuevaDarya <zuevadasha55@gmail.com>
2024-07-28 21:49:34 +0300

    поправила стили для отображения шапки на странице профиля

--- packages/client/src/components/organisms/Header.tsx
+++ packages/client/src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from '../molecules/Navbar';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 px-4 lg:px-6 h-14 flex items-center bg-[#0c1b2a]">
+    <header className="sticky z-10 top-0 px-4 lg:px-6 h-14 flex items-center bg-[#0c1b2a]">
       <Link to="/" variant="withIcon">
         <RocketIcon className="size-6 text-[#ffc107]" />
         <span className="text-white">Capybara Crusaders</span>
--- packages/client/src/pages/Profile.tsx
+++ packages/client/src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import ProfileSection from '../components/organisms/ProfileSection';
 
 export default function Profile() {
   return (
-    <main className="relative w-full min-h-dvh p-4 pt-10 bg-[#152f48] py-5">
+    <main className="relative w-full min-h-[calc(100vh-3.5rem)] p-4 pt-10 bg-[#152f48] py-5">
       <ProfileSection />
     </main>
   );
