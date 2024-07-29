6267aa3e9703c7bab96809723fca14789f3812cf
Mikhail Khmelnitskiy <mikhail.khmel@gmail.com>
2024-07-29 21:29:48 +0700

    сверстал страницу начала игры

--- packages/client/src/components/atoms/Button.tsx
+++ packages/client/src/components/atoms/Button.tsx
@@ -1,8 +1,6 @@
 import { VariantProps, cva } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
-import { cn } from './../../lib/utils';
-
 const buttonVariants = cva('', {
   variants: {
     variant: {
@@ -11,13 +9,14 @@ const buttonVariants = cva('', {
         'bg-transparent inline-flex items-center justify-center rounded-md border border-red-600 p-2 text-sm font-medium text-white shadow transition-colors hover:bg-red-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
       acentNotTransparent:
         'inline-flex items-center justify-center rounded-md border border-red-600 p-2 text-sm font-medium text-white shadow transition-colors hover:bg-red-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+      contained:
+        'inline-flex h-10 items-center justify-center rounded-md bg-[#ffc107] px-8 text-sm font-medium text-[#0c1b2a] shadow transition-colors hover:bg-[#ffdd5c] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
     },
   },
   defaultVariants: {
     variant: 'default',
   },
 });
-
 
 interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
