e190b500dd5494ade7568c48a7ae159e24f205b4
Ворожцов Артем Ильич <a_vorozhtsov@step.ru>
2024-08-03 03:37:21 +0300

    [FEAT]: добавил страницу логина, висит баг с переходом по ссылке регистрации

---
+++ packages/client/src/components/organisms/LoginSection.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Form from './Form';
+import Subtitle from '../atoms/Subtitle';
+import { LoginFormData } from '../../types/types';
+import { LoginValidationSchema } from '../../types/validationSchemas';
+import registrationCapibara from '../../../public/registrationCapibara.webp';
+import registrationCapibara2 from '../../../public/registrationCapibara2.webp';
+import FORM_INPUT_NAMES from '../../constants/formInputNames';
+import Link from '../atoms/Link';
+
+const LoginSection: React.FC = () => {
+  const handleLogin = (data: LoginFormData) => {
+    console.log('Login Data:', data);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 flex flex-col">
+      <Subtitle className="text-[#ffc107]" as="h2" variant="h2">
+        Войти
+      </Subtitle>
+      <Form<LoginFormData>
+        zodSchema={LoginValidationSchema}
+        onSubmit={handleLogin}
+        buttonText="Submit"
+        buttonVariant="acentNotTransparent"
+        buttonClass="bg-[#ffc107]"
+        formFieldClass="mb-4 "
+        labelVariant="basic"
+        inputVariant="basic"
+        fields={[
+          { name: FORM_INPUT_NAMES.login, label: 'Логин' },
+          {
+            name: FORM_INPUT_NAMES.password,
+            label: 'Пароль',
+            type: 'password',
+          },
+        ]}
+      />
+      <Link className="self-center mt-10" to="/registration">
+        Зарегистрироваться
+      </Link>
+      <img
+        src={registrationCapibara}
+        width="225"
+        alt="Космическая капибара!"
+        className="mx-auto aspect-square overflow-hidden rounded-xl object-cover absolute top-0 right-0 opacity-50"
+      />
+      <img
+        src={registrationCapibara2}
+        width="225"
+        alt="Космическая капибара!"
+        className="absolute top-10 left-0 opacity-50"
+      />
+    </div>
+  );
+};
+
+export default LoginSection;
--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -1,3 +1,9 @@
+import LoginSection from '../components/organisms/LoginSection';
+
 export default function Login() {
-  return <>Аутентификация</>;
+  return (
+    <main className="flex flex-col min-h-dvh bg-[#0c1b2a] py-5">
+      <LoginSection />
+    </main>
+  );
 }
