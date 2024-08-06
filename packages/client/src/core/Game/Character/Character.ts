090ccba113d5440ced7055becf28346095f1658a
D33Key <dimakisa777@yandex.ru>
2024-08-07 00:59:36 +0300

    [FEAT]: добавил отображение щита

--- packages/client/src/core/Game/Character/Character.ts
+++ packages/client/src/core/Game/Character/Character.ts
@@ -113,7 +113,42 @@ export default abstract class Character {
       );
 
       this.drawHealthBar(context, this.x, this.y * 3, this.width, 20);
+      this.drawShield(context, this.x, this.y * 3.3, 50, 50);
     }
+  }
+
+  drawShield(
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
+    context.beginPath();
+    context.moveTo(x + width / 2, y + height);
+    context.lineTo(x + width, y + height * 0.6);
+    context.lineTo(x + width, y);
+    context.lineTo(x, y);
+    context.lineTo(x, y + height * 0.6);
+    context.closePath();
+
+    context.fillStyle = '#709CCB';
+    context.fill();
+
+    context.lineWidth = 2;
+    context.strokeStyle = '#003366';
+    context.stroke();
+
+    const fontSize = 24;
+    context.font = `bold ${fontSize}px Arial`;
+    context.fillStyle = '#ffffff';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+
+    const textX = x + width / 2;
+    const textY = y + (height * 0.6 + height * 0.4) / 2;
+
+    context.fillText(String(this.resist + this.tempResist), textX, textY);
   }
 
   addCardInHand(card: CardInHand) {
