2ce81f854be9622e90819ec71dc724bb4b0ed495
ZuevaDarya <zuevadasha55@gmail.com>
2024-08-08 19:12:21 +0300

    [REFACTOR]: почистила методы

--- packages/client/src/startServiceWorker.ts
+++ packages/client/src/startServiceWorker.ts
@@ -2,7 +2,7 @@ export default function startServiceWorker() {
   if (navigator.serviceWorker) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/sw.js', { scope: '/' })
+        .register('cacheServiceWorker.js', { scope: '/' })
         .then(() => console.log('[SW] register'))
         .catch(error => console.log('[SW] register failed: ', error));
     });
