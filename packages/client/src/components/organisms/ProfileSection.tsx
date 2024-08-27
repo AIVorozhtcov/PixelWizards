4aca6199dd02d873cf8a26990c737a4f5bba48cc
D33Key <dimakisa777@yandex.ru>
2024-08-27 15:41:48 +0300

    [REFACTOR]: переделал профиль, убарл ненужный ререндер при открытии попапа

--- packages/client/src/components/organisms/ProfileSection.tsx
+++ packages/client/src/components/organisms/ProfileSection.tsx
@@ -1,18 +1,13 @@
 import { useState } from 'react';
 import generalAPI from '../../api/fetchTransport/generalApi';
-import { PROFILE_MODE, PROFILE_POPUP } from '../../constants/profilePageData';
-import { FormAvatarType, ProfileModeType } from '../../types/types';
+import { PROFILE_POPUP } from '../../constants/profilePageData';
+import { FormAvatarType } from '../../types/types';
 import { ProfileUpdateAvatarSchema } from '../../types/validationSchemas';
-import Button from '../atoms/Button';
 import Popup from '../molecules/Popup';
 import ProfileAvatar from '../molecules/ProfileAvatar';
-import ProfileModeManager from './ProfileModeManager';
 
-const ProfileSection = () => {
-  const [mode, setMode] = useState<ProfileModeType>('base');
+const ProfileSection = ({ children }: { children: React.ReactNode }) => {
   const [isPopupDisplay, setPopupDisplay] = useState<boolean>(false);
-
-  const handleClick = () => setMode(PROFILE_MODE.base);
 
   return (
     <>
@@ -27,19 +22,9 @@ const ProfileSection = () => {
           popup={PROFILE_POPUP}
         />
       )}
-      {mode !== PROFILE_MODE.base && (
-        <Button
-          variant="red"
-          className="absolute top-4 left-4"
-          onClick={handleClick}>
-          Закрыть
-        </Button>
-      )}
       <div className="flex flex-col items-center gap-6 max-w-lg min-w-80 my-0 mx-auto">
         <ProfileAvatar handleClick={() => setPopupDisplay(true)} />
-        <div className="w-full flex flex-col gap-10">
-          <ProfileModeManager mode={mode} setMode={setMode} />
-        </div>
+        {children}
       </div>
     </>
   );
--- packages/client/src/pages/Profile.tsx
+++ packages/client/src/pages/Profile.tsx
@@ -1,9 +1,14 @@
+import ProfileModeManager from '../components/organisms/ProfileModeManager';
 import ProfileSection from '../components/organisms/ProfileSection';
 
 export default function Profile() {
   return (
     <main className="relative w-full min-h-[calc(100vh-3.5rem)] p-4 pt-10 bg-[#152f48] py-5">
-      <ProfileSection />
+      <ProfileSection>
+        <div className="w-full flex flex-col gap-10">
+          <ProfileModeManager />
+        </div>
+      </ProfileSection>
     </main>
   );
 }
