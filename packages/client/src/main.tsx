104c643867bfbc2ff7da7859c63e99544bbfc3c3
Mikhail Khmelnitskiy <mikhail.khmel@gmail.com>
2024-08-29 22:34:47 +0300

    [FIX]: вернул файлы и команды для классического запуска проекта

---
+++ packages/client/src/main.tsx
@@ -0,0 +1,17 @@
+import '../index.css';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './constants/router';
+import startServiceWorker from './startServiceWorker';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </>
+);
+
+startServiceWorker();
