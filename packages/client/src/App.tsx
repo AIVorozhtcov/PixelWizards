0fa624071e74833994fd96c4453cd46374df41e8
Mikhail Khmelnitskiy <mikhail.khmel@gmail.com>
2024-08-29 23:08:02 +0300

    [FIX]: исправление тестов

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,5 @@
-import Test from './__test__';
-
 function App() {
-  return (
-    <div className="text-3xl font-bold">
-      Hello world <Test />
-    </div>
-  );
+  return <div className="text-3xl font-bold">Hello world</div>;
 }
 
 export default App;
