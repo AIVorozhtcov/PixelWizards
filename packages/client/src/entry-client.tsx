624dc68d6bbc15fbeee8de60d71103b6d42b1bc9
Mikhail Khmelnitskiy <mikhail.khmel@gmail.com>
2024-09-17 15:56:28 +0300

    [FIX]: удаление лишнего

--- packages/client/src/entry-client.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import '../index.css';
-import App from './App';
-import startServiceWorker from './startServiceWorker';
-
-ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />);
-
-startServiceWorker();
