12279c464eef8f6ac12eb53dba0de26a309ee782
D33Key <dimakisa777@yandex.ru>
2024-09-17 10:05:58 +0300

    [FIX]: чиню тест

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -18,7 +18,7 @@ export const sequelize = new Sequelize(sequelizeOptions);
 export async function dbConnect() {
   try {
     await sequelize.authenticate(); // Проверка аутентификации в БД
-    await sequelize.sync(); // Синхронизация базы данных
+    await sequelize.sync({ alter: true }); // Синхронизация базы данных
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
