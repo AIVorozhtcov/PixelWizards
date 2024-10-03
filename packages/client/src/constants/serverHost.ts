d65b0040b33114c737840b15f7b43414c604da76
D33Key <dimakisa777@yandex.ru>
2024-10-03 12:42:33 +0300

    [FIX]: заменил порт

--- packages/client/src/constants/serverHost.ts
+++ packages/client/src/constants/serverHost.ts
@@ -3,7 +3,7 @@ import '../../client.d';
 export const SERVER_HOST =
   typeof window === 'undefined'
     ? 'http://server:3001'
-    : 'http://158.160.21.142:3001';
+    : 'http://158.160.21.142:3000';
 // export const SERVER_HOST =
 //   typeof window === 'undefined'
 //     ? __INTERNAL_SERVER_URL__
